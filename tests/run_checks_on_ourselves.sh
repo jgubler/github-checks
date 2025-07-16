b0a5fb59df407d8cf66780a6c3d4deb17e5f0bc4
Julian Gubler <mail@juliangubler.de>
2025-07-16 09:15:07 +0200

    feat: add check-jsonschema formatter

--- tests/run_checks_on_ourselves.sh
+++ tests/run_checks_on_ourselves.sh
@@ -2,7 +2,7 @@
 
 # set up environment variables up front
 export GH_APP_ID="1065841" GH_APP_INSTALL_ID="57443831"
-export GH_PRIVATE_KEY_PEM="/Users/jgubler/.ssh/demo-github-checks-app-pkcs8.pem"
+export GH_PRIVATE_KEY_PEM="<>"
 export GH_REPO_BASE_URL="https://github.com/jgubler/github-checks"
 export GH_CHECK_REVISION="$(git rev-parse HEAD)"
 export GH_LOCAL_REPO_PATH="/Users/jgubler/repos/github-checks"
@@ -13,32 +13,28 @@ python3 -m venv .temp_venv
 . .temp_venv/bin/activate
 python3 -m pip install -e .[dev]
 
-# initialize & start a check run
+# initialize the checks app to auth with GitHub
 python3 -m github_checks.cli init --overwrite-existing
-python3 -m github_checks.cli start-check-run --check-name ruff-checks
 
 # run ruff on ourselves
+python3 -m github_checks.cli start-check-run --check-name ruff-checks
 ruff check . --output-format=json > ruff_output.json
-
-# finish the check run, passing ruff's errors as annotations to GitHub
 python3 -m github_checks.cli finish-check-run ruff_output.json --log-format ruff-json --no-cleanup
 
-python3 -m github_checks.cli start-check-run --check-name mypy-checks
-
 # run mypy on ourselves
+python3 -m github_checks.cli start-check-run --check-name mypy-checks
 mypy . --output=json > mypy_output.json
-
-# finish the check run, passing mypy's errors as annotations to GitHub
 python3 -m github_checks.cli finish-check-run mypy_output.json --log-format mypy-json --no-cleanup
 
-
-python3 -m github_checks.cli start-check-run --check-name raw-checks
-
-# run mypy on ourselves
+# random raw output to test the raw log format
+python3 -m github_checks.cli start-check-run --check-name test-raw-checks
 echo "I just don't like this code" > raw_output.json
+python3 -m github_checks.cli finish-check-run raw_output.json --log-format raw --no-cleanup
 
-# finish the check run, passing mypy's errors as annotations to GitHub
-python3 -m github_checks.cli finish-check-run raw_output.json --log-format raw
+# random raw output to test the raw log format
+python3 -m github_checks.cli start-check-run --check-name test-jsonschema-checks
+check-jsonschema -o json --schemafile tests/json_validation/user.schema.json tests/json_validation/invalid_user.json > jsonschema_output.json
+python3 -m github_checks.cli finish-check-run jsonschema_output.json --log-format check-jsonschema
 
 # clean up the venv
 deactivate
